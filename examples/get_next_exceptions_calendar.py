9a96a6d6af01308435ba0652ce112adf79578847
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-06-02 15:35:16 +0000

    small improvements made for ha

---
+++ examples/get_next_exceptions_calendar.py
@@ -0,0 +1,25 @@
+"""Retrieves next exception for each service category."""
+
+from __future__ import annotations
+
+import asyncio
+
+import aiohttp
+from nyc311calendar import CalendarType
+from nyc311calendar import NYC311API
+
+API_KEY = "YOUR_API_KEY"
+
+
+async def main() -> None:
+    """Retrieve and print calendar."""
+    async with aiohttp.ClientSession() as session:
+        calendar = NYC311API(session, API_KEY)
+        resp = await calendar.get_calendar(calendars=[CalendarType.NEXT_EXCEPTIONS])
+
+        print(resp)
+
+    await session.close()
+
+
+asyncio.run(main())
